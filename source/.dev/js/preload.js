ab5cf8eb5a275c0f08747a23985bce45fd7a4a4a
Hyun <1286252638@qq.com>
2021-03-01 15:43:18 +0800

    fix unable to load theme config

--- source/.dev/js/preload.js
+++ source/.dev/js/preload.js
@@ -1,12 +1,11 @@
 (function(){
-    var setProp = function(...p){document.documentElement.style.setProperty(...p)};
-    if(localStorage.getItem("nightMode")==='true') setProp('data-theme', 'dark');
+    if(localStorage.getItem("nightMode")==='true') document.documentElement.setAttribute('data-theme', 'dark');
     var themeColor = localStorage.getItem("themeColor");
     if(themeColor!=undefined) {
         console.log('[*] 检测到本地主题色：', themeColor);
-        setProp('--theme-color',themeColor);
+        document.documentElement.style.setProperty('--theme-color',themeColor);
     } else if('default_theme' in window){
         var temp = parseInt(default_theme.substring(1),16);
-        setProp('--theme-color',`${(temp>>16)&255},${(temp>>8)&255},${temp&255}`);
+        document.documentElement.style.setProperty('--theme-color',`${(temp>>16)&255},${(temp>>8)&255},${temp&255}`);
     }
 })();
